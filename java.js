97371686fe39928bf5e2912232bea9b708876579
01792166684 <tanzinatania491991@gmail.com>
2022-09-03 00:21:34 +0600

    second commit

--- java.js
+++ java.js
@@ -1,1 +1,18 @@
-console.log('here')
+const loadMedias =()=>{
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res=>res.json())
+    .then(data=>displayMedias(data.data.news_category))
+}
+const displayMedias = medias=>{
+    console.log(medias);
+     const categoryContainer = document.getElementById('category-container');
+     medias.forEach(media =>{
+        console.log(media.category_name)
+        const categoryList = document.createElement('li');
+        categoryList.innerHTML= `<button class="p-8">${media.category_name}</button>`;
+    categoryContainer.appendChild(categoryList)
+     }
+        )
+}
+
+    loadMedias();
